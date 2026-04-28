06899d584cdbfdb41978a78fa846af8fb04ebddf
myewkew-dot <myewkew@gmail.com>
2026-04-28 20:17:39 +0800

    Update kew_custom_s3.cc

--- main/boards/kew-custom-s3/kew_custom_s3.cc
+++ main/boards/kew-custom-s3/kew_custom_s3.cc
@@ -1,16 +1,15 @@
 #include "boards/kew-custom-s3/config.h"
 #include "boards/common/board.h"
-#include "boards/common/wifi_board.h" // <--- This is the missing piece!
+#include "boards/common/wifi_board.h"
 
 class KewCustomS3Board : public WifiBoard {
 public:
     KewCustomS3Board() {
-        // The board uses the standard WifiBoard implementation 
-        // with the pins defined in config.h
+        // Board uses standard WifiBoard implementation with custom pins
     }
 };
 
-extern "C" const Board* get_board() {
+extern "C" const Board* create_board() {
     static KewCustomS3Board board;
     return &board;
 }
